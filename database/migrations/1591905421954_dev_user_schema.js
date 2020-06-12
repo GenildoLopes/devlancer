56ccfb64cf570e9a03b60c0e8103ddfa5366fb62
[GenildoLopes] <[carvalhogenildo800@gmail.com]>
2020-06-11 21:58:13 -0300

    Create Table for "UserDevs"

--- database/migrations/1591905421954_dev_user_schema.js
+++ database/migrations/1591905421954_dev_user_schema.js
@@ -7,6 +7,15 @@ class DevUserSchema extends Schema {
   up () {
     this.create('dev_users', (table) => {
       table.increments()
+      table.string('nome', 50).notNullable().unique()
+      table.string('email', 60).notNullable().unique()
+      table.string('senha', 20).notNullable()
+      table.string('país', 20).notNullable()
+      table.string('estado', 20).notNullable()
+      table.string('área', 30)
+      table.text('descrição', 200)
+      table.text('habilidades', 200)
+      table.integer('pontos', 6)
       table.timestamps()
     })
   }
